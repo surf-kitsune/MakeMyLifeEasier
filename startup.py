1b111acef5904aeca972369aaeea40c1216f2200
hyper <47005482+Hali-fax@users.noreply.github.com>
2022-01-30 00:47:48 -1000

    Open all my startup programs
    I finally get to decide whether I want to open them or not....

---
+++ startup.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+subprocess.call(['C:\Program Files\Mozilla Firefox\\firefox.exe', '-new-tab'])
+os.startfile(r"C:\Users\Battlestation\AppData\Roaming\Spotify\Spotify.exe")
+os.startfile(r"C:\Users\Battlestation\AppData\Local\Discord\app-1.0.9003\Discord.exe")
+
+# os.startfile
